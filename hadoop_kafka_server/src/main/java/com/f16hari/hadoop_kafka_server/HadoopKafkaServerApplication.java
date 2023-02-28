bf4f26f8c2401fd336e77b5d22a67d732b7feb09
Hariharan Jeyachandran <jhariharan99official@gmail.com>
2023-02-28 13:17:45 +0530

    project initialized

---
+++ hadoop_kafka_server/src/main/java/com/f16hari/hadoop_kafka_server/HadoopKafkaServerApplication.java
@@ -0,0 +1,13 @@
+package com.f16hari.hadoop_kafka_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HadoopKafkaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HadoopKafkaServerApplication.class, args);
+	}
+
+}
---
+++ hadoop_kafka_server/src/test/java/com/f16hari/hadoop_kafka_server/HadoopKafkaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f16hari.hadoop_kafka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HadoopKafkaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
